6495efa605ca3429f9aeea92665539c729e11771
IstrateGabi <72501829+IstrateGabi@users.noreply.github.com>
2020-12-11 13:56:56 +0200

    Add files via upload

---
+++ Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Istrate_Gabriel_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
